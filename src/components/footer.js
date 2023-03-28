4eb967d8df6f55121cec04207185001de0e76473
Sabina Ceja-Siete <sabinasiete@gmail.com>
2023-03-28 06:57:32 -0700

    simplified files

---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+import me2 from "../../assets/me1.jpg";
+import "../style/about.css";
+function About(props) {
+  return (
+    <div className="wrapper">
+      <div className="about--container">
+        <div className="about--heading">
+          <h3>Hello!</h3>
+          <h4>I am Sabina</h4>
+        </div>
+        <div className="about--img">
+          <img src={me1} alt="about" />
+        </div>
+        <div className="about--text">
+          <p>
+            {" "}
+            Developer who is just getting
+            started with this exciting technology. I am an avid learner and always looking to expand my knowledge and expertise in the field of web development.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/components/pages/portfolio/Portfolio_data.js
+++
@@ -1,1 +0,0 @@
-import placeholder from "../../../assets/placeholder.png";
